1f1352f5fe56ffa315eeb1b2dd4407f0c44245d2
Denis P. Rails Course <poldens@gmail.com>
2018-03-09 19:10:08 +0000

    add admin and navigation partial and update navbar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,7 @@ class RegistrationsController < Milia::RegistrationsController
     # make any changes
     
     tenant_params = sign_up_params_tenant
-    user_params = sign_up_params_user
+    user_params = sign_up_params_user.merge({ is_admin: true })
     coupon_params = sign_up_params_coupon
     
     sign_out_session!
